28a39899eb9747ca56c485594f2d24c1577f514e
Metelkov Sergey <zena@MacBook-Pro-Maksim.local>
2020-11-08 21:28:50 +0300

    highest_building

---
+++ checkio/github/highest_building.py
@@ -0,0 +1,56 @@
+def highest_building(buildings):
+
+    temp_build_size = 0
+    building_size_list = []
+
+    buildings_count = len(buildings[0])
+    for i in range(buildings_count):
+        for build in buildings:
+            temp_build_size += build[i]
+        building_size_list.append([i+1, temp_build_size])
+        temp_build_size = 0
+
+    building_size_list.sort(key=lambda x: x[1], reverse=True)
+
+    return building_size_list[0]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(highest_building([
+        [0, 0, 1, 0],
+        [1, 0, 1, 0],
+        [1, 1, 1, 0],
+        [1, 1, 1, 1]
+    ]))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert highest_building([
+        [0, 0, 1, 0],
+        [1, 0, 1, 0],
+        [1, 1, 1, 0],
+        [1, 1, 1, 1]
+    ]) == [3, 4], "Common"
+    assert highest_building([
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 1]
+    ]) == [4, 1], "Cabin in the wood"
+    assert highest_building([
+        [1, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0],
+        [1, 1, 1, 0, 0],
+        [1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1]
+    ]) == [1, 5], "Triangle"
+    assert highest_building([
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1]
+    ]) == [4, 6], "Pyramid"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
